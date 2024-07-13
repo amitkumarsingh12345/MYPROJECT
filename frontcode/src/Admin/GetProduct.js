ab3673eb0ce0f87bd4c8d824eb36fe8654f3768d
amitkumarsingh12345 <163759129+amitkumarsingh12345@users.noreply.github.com>
2024-07-12 22:58:03 -0700

    Delete frontcode directory
    xyz

--- frontcode/src/Admin/GetProduct.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const viewproductHandler = async () => {
-    let data = await axios.get('http://localhost:11000/product');
-    data = data?.data;
-    return data;
-}
--- frontcode/src/Admin/ProtectedAdmin.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom';
-
-const ProtectedAdmin = () => {
-    const admin = localStorage.getItem('admin');
-    return admin ? <Outlet /> : <Navigate to={'/'} />
-}
-
-export default ProtectedAdmin;
